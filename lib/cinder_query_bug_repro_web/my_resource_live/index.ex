6bcb1170230d8f481431a279db6e2fe8d357f3cb
Egor Kislitsyn <egor@kislitsyn.com>
2025-09-21 03:22:03 +0400

    Add multiple table views to test query options

--- lib/cinder_query_bug_repro_web/my_resource_live/index.ex
+++ lib/cinder_query_bug_repro_web/my_resource_live/index.ex
@@ -1,5 +1,6 @@
 defmodule CinderQueryBugReproWeb.MyResourceLive.Index do
   use CinderQueryBugReproWeb, :live_view
+  require Ash.Query
 
   alias CinderQueryBugRepro.MyDomain.MyResource
 
@@ -8,15 +9,43 @@ defmodule CinderQueryBugReproWeb.MyResourceLive.Index do
     ~H"""
     <Layouts.app flash={@flash}>
       <.header>
-        Listing My resources
+        Listing All
+      </.header>
+
+      <Cinder.Table.table resource={MyResource}>
+        <:col :let={my_resource} field="number" sort>{my_resource.number}</:col>
+        <:col :let={my_resource} field="is_active" sort>{my_resource.is_active}</:col>
+      </Cinder.Table.table>
+
+      <.header>
+        Listing with<br />
+        <code class="text-primary">
+          query_opts=&lbrace;[filter: [is_active: true], sort: :number]}
+        </code>
       </.header>
 
       <Cinder.Table.table
         resource={MyResource}
-        id="my_resources"
+        id="my_resources_active"
+        query_opts={[filter: [is_active: true], sort: :number]}
       >
-        <:col :let={my_resource} field="number" filter sort>{my_resource.number}</:col>
-        <:col :let={my_resource} field="is_active" filter sort>{my_resource.is_active}</:col>
+        <:col :let={my_resource} field="number" sort>{my_resource.number}</:col>
+        <:col :let={my_resource} field="is_active" sort>{my_resource.is_active}</:col>
+      </Cinder.Table.table>
+
+      <.header>
+        Listing with:<br />
+        <code class="text-primary">
+          query=&lbrace;MyResource |> Ash.Query.filter(is_active: true) |> Ash.Query.sort(:number)}
+        </code>
+      </.header>
+
+      <Cinder.Table.table
+        query={MyResource |> Ash.Query.filter(is_active: true) |> Ash.Query.sort(:number)}
+        id="my_resources_query_direct"
+      >
+        <:col :let={my_resource} field="number" sort>{my_resource.number}</:col>
+        <:col :let={my_resource} field="is_active" sort>{my_resource.is_active}</:col>
       </Cinder.Table.table>
     </Layouts.app>
     """
