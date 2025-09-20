3c355201cf2d2a76c447c8eb9b521db379f853ff
Egor Kislitsyn <egor@kislitsyn.com>
2025-09-21 02:36:10 +0400

    Add seed data with number and activity fields

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,6 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     CinderQueryBugRepro.Repo.insert!(%CinderQueryBugRepro.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+for i <- 1..10 do
+  Ash.Seed.seed!(CinderQueryBugRepro.MyDomain.MyResource, %{
+    number: i,
+    is_active: rem(i, 2) == 0
+  })
+end
